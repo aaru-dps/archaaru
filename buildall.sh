79797aa959bfaf5570debe9364702a7b39bd9ecb
Natalia Portillo <claunia@claunia.com>
2019-10-26 12:09:06 +0100

    Added script to build both ISOs

---
+++ buildall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd archdic-x86
+./build.sh -v -N dicarch -L DIC_`date +%Y%m` -P "Natalia Portillo <claunia@claunia.com>" -A "DiscImageChef Rescue CD"
+cd ../archdic-x86_64
+./build.sh -v -N dicarch -L DIC_`date +%Y%m` -P "Natalia Portillo <claunia@claunia.com>" -A "DiscImageChef Rescue CD"
+cd ..
