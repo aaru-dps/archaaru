9ce93d3752928548a89ef4474b3b81ac97bbcfc1
Natalia Portillo <claunia@claunia.com>
2020-03-01 15:05:56 +0000

    Rename

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd archdic-x86
-./build.sh -v -N dicarch -L DIC_`date +%Y%m` -P "Natalia Portillo <claunia@claunia.com>" -A "DiscImageChef Rescue CD"
+./build.sh -v -N dicarch -L DIC_`date +%Y%m` -P "Natalia Portillo <claunia@claunia.com>" -A "Aaru Rescue CD"
 cd ../archdic-x86_64
-./build.sh -v -N dicarch -L DIC_`date +%Y%m` -P "Natalia Portillo <claunia@claunia.com>" -A "DiscImageChef Rescue CD"
+./build.sh -v -N dicarch -L DIC_`date +%Y%m` -P "Natalia Portillo <claunia@claunia.com>" -A "Aaru Rescue CD"
 cd ..
